669e286b1cc27579087a7733d72f6421a978d083
FabFisk <fischettofabrizio@gmail.com>
2016-11-23 13:35:56 +0100

    Filtro fixed

--- TestFiltri/src/main/java/it/alfasoft/fabrizio/controller/LoginController.java
+++ TestFiltri/src/main/java/it/alfasoft/fabrizio/controller/LoginController.java
@@ -1,7 +1,9 @@
 package it.alfasoft.fabrizio.controller;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 @ManagedBean(name="loginController")
 @SessionScoped
@@ -9,9 +11,18 @@ public class LoginController {
 	
 	private String username;
 	private String password;
+	private boolean loggato;
 	
+	public LoginController(){
+		loggato = false;
+	}
 	
-	
+	public boolean isLoggato() {
+		return loggato;
+	}
+	public void setLoggato(boolean loggato) {
+		this.loggato = loggato;
+	}
 	public String getUsername() {
 		return username;
 	}
@@ -23,6 +34,21 @@ public class LoginController {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public String doLogin(){
+		if(!this.username.equals("aaa") 
+				|| !this.password.equals("aaa")){
+			//se non è verificato, rimando su login page
+			FacesMessage msg = new FacesMessage("Username e/o Password errate!");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+			
+			return "Login";			
+		}		
+		//se è verificato, posso mandare su home page
+		this.loggato = true;
+		return "/portali/home?faces-redirect=true";
+		
 	}
 	
 	
